fb1857085f370d9f262cef05ba4781afa3370735
clientesweb <alexisifran32@gmail.com>
2025-01-20 18:29:18 -0300

    Actualizar page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,34 @@
-import TopBanner from '../components/TopBanner'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import FeaturedPrograms from '../components/FeaturedPrograms'
-import LiveStream from '../components/LiveStream'
-import Shorts from '../components/Shorts'
-import AdBanner from '../components/AdBanner'
-import Sponsors from '../components/Sponsors'
-import DownloadApp from '../components/DownloadApp'
-import Footer from '../components/Footer'
-import BottomNav from '../components/BottomNav'
-import FloatingChat from '../components/FloatingChat'
+import dynamic from "next/dynamic"
+import TopBanner from "../components/TopBanner"
+import Header from "../components/Header"
+import Hero from "../components/Hero"
+import FeaturedPrograms from "../components/FeaturedPrograms"
+
+const LiveStream = dynamic(() => import("../components/LiveStream"))
+const Shorts = dynamic(() => import("../components/Shorts"))
+const AdBanner = dynamic(() => import("../components/AdBanner"))
+const Sponsors = dynamic(() => import("../components/Sponsors"))
+const DownloadApp = dynamic(() => import("../components/DownloadApp"))
+const Footer = dynamic(() => import("../components/Footer"))
+const BottomNav = dynamic(() => import("../components/BottomNav"))
+const FloatingChat = dynamic(() => import("../components/FloatingChat"))
 
 export default function Home() {
   return (
-    <>
+    <div>
       <TopBanner />
       <Header />
-      <main className="flex-grow">
-        <Hero />
-        <FeaturedPrograms />
-        <LiveStream />
-        <Shorts />
-        <AdBanner />
-        <Sponsors />
-        <DownloadApp />
-      </main>
+      <Hero />
+      <FeaturedPrograms />
+      <LiveStream />
+      <Shorts />
+      <AdBanner />
+      <Sponsors />
+      <DownloadApp />
       <Footer />
       <BottomNav />
       <FloatingChat />
-    </>
+    </div>
   )
 }
 
