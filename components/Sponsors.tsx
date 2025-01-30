db57d6a98c53e962270def62cd90f9a35c941d9e
clientesweb <alexisifran32@gmail.com>
2025-01-30 16:19:10 -0300

    Actualizar Sponsors.tsx

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -10,6 +10,7 @@ const sponsors = [
   { id: 4, src: '/images/sponsor4.png', alt: 'Sponsor 4' },
   { id: 5, src: '/images/sponsor5.png', alt: 'Sponsor 5' },
   { id: 6, src: '/images/sponsor6.png', alt: 'Sponsor 6' },
+  { id: 7, src: '/images/sponsor6.png', alt: 'Sponsor 7' },
 ]
 
 export default function Sponsors() {
