fa1d93879c6a6e40b229627f1f4cd77fd30a8aa4
clientesweb <alexisifran32@gmail.com>
2025-02-28 00:48:44 -0300

    Actualizar page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,32 @@
-import TopBanner from '../components/TopBanner'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import FeaturedPrograms from '../components/FeaturedPrograms'
-import LiveStream from '../components/LiveStream'
-import Shorts from '../components/Shorts'
-import AdBanner from '../components/AdBanner'
-import Sponsors from '../components/Sponsors'
-import DownloadApp from '../components/DownloadApp'
-import Footer from '../components/Footer'
-import BottomNav from '../components/BottomNav'
-import FloatingChat from '../components/FloatingChat'
+"use client"
+
+import { ErrorBoundary } from "react-error-boundary"
+import TopBanner from "../components/TopBanner"
+import Header from "../components/Header"
+import Hero from "../components/Hero"
+import FeaturedPrograms from "../components/FeaturedPrograms"
+import LiveStream from "../components/LiveStream"
+import Shorts from "../components/Shorts"
+import AdBanner from "../components/AdBanner"
+import Sponsors from "../components/Sponsors"
+import DownloadApp from "../components/DownloadApp"
+import Footer from "../components/Footer"
+import BottomNav from "../components/BottomNav"
+import FloatingChat from "../components/FloatingChat"
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
-    <>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
       <TopBanner />
       <Header />
       <main className="flex-grow">
@@ -28,6 +41,7 @@ export default function Home() {
       <Footer />
       <BottomNav />
       <FloatingChat />
-    </>
+    </ErrorBoundary>
   )
 }
+
