36f7bf0b48b6ee289213299e8f3ec85aa906018e
clientesweb <alexisifran32@gmail.com>
2025-03-22 21:57:40 -0300

    Actualizar page.tsx

--- app/contenido/page.tsx
+++ app/contenido/page.tsx
@@ -1,19 +1,19 @@
-import TopBanner from "../../components/TopBanner"
-import Header from "../../components/Header"
-import FeaturedPrograms from "../../components/FeaturedPrograms"
-import LiveStream from "../../components/LiveStream"
-import Shorts from "../../components/Shorts"
-import Footer from "../../components/Footer"
-import BottomNav from "../../components/BottomNav"
-import FloatingChat from "../../components/FloatingChat"
-import ContentHero from "../../components/ContentHero"
+import TopBanner from "@/components/TopBanner"
+import Header from "@/components/Header"
+import FeaturedPrograms from "@/components/FeaturedPrograms"
+import LiveStream from "@/components/LiveStream"
+import Shorts from "@/components/Shorts"
+import Footer from "@/components/Footer"
+import BottomNav from "@/components/BottomNav"
+import FloatingChat from "@/components/FloatingChat"
+import ContentHero from "@/components/ContentHero"
 
 export default function ContenidoPage() {
   return (
     <>
       <TopBanner />
       <Header />
-      <main className="flex-grow">
+      <main id="main-content" className="flex-grow">
         <ContentHero />
         <FeaturedPrograms />
         <LiveStream showPlaylist={true} />
