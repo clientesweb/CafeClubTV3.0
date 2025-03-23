9752557c24d97c64ab05e0bf0aaf9a76834ef4fa
clientesweb <alexisifran32@gmail.com>
2025-03-22 21:14:23 -0300

    Create page.tsx

---
+++ app/contenido/page.tsx
@@ -0,0 +1,28 @@
+import TopBanner from "../../components/TopBanner"
+import Header from "../../components/Header"
+import FeaturedPrograms from "../../components/FeaturedPrograms"
+import LiveStream from "../../components/LiveStream"
+import Shorts from "../../components/Shorts"
+import Footer from "../../components/Footer"
+import BottomNav from "../../components/BottomNav"
+import FloatingChat from "../../components/FloatingChat"
+import ContentHero from "../../components/ContentHero"
+
+export default function ContenidoPage() {
+  return (
+    <>
+      <TopBanner />
+      <Header />
+      <main className="flex-grow">
+        <ContentHero />
+        <FeaturedPrograms />
+        <LiveStream showPlaylist={true} />
+        <Shorts />
+      </main>
+      <Footer />
+      <BottomNav activeItem="contenido" />
+      <FloatingChat />
+    </>
+  )
+}
+
